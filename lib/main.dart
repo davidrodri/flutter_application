f6caffa33b9597c1a4927b7dae86accf9245258b
David Rodríguez <david_rodriguezsuarez@hotmail.com>
2021-12-16 21:17:44 -0600

    Change homescreen in own widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Center(
-          child: Text('Hi World!'),
-        ));
+        debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
